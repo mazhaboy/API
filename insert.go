2f73337107a595484fe42d1ca479aaf186e9a8ce
mazhaboy <160103098@stu.sdu.edu.kz>
2020-08-27 01:13:19 +0600

    api

--- insert.go
+++ insert.go
@@ -3,25 +3,46 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type a struct {
+	id       int
+	name     string
+	surename string
+	age      int
+}
+
 func main() {
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/mazha")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/kv108")
 
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	result, err := db.Exec("insert into dailycheklist (date, done) values (?, ?)",
-		"25.08.2020", "UDEMY COURSES: WORKING WITH MYSQL")
+	rows, err := db.Query("select *from kv108")
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err)
 	}
-	fmt.Println(result.LastInsertId()) // id добавленного объекта
-	fmt.Println(result.RowsAffected()) // количество затронутых строк
+	reads := []a{}
+	for rows.Next() {
+		data := a{}
+		err := rows.Scan(&data.id, &data.name, &data.surename, &data.age)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		reads = append(reads, data)
+
+	}
+	// for _, row := range reads {
+	// 	fmt.Println(row)
+	// }
+	fmt.Println(reads)
+
 }
 
 // create table products (id int auto_increment primary key, name v
